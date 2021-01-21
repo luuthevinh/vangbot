65e8b37a882b1c3101a739d5e910fb551be6d992
Vinh Luu <luuthevinh7@gmail.com>
2021-01-21 11:44:38 +0700

    + get image

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 const TeleBot = require('telebot');
+const https = require('https');
 
 const TELEGRAM_BOT_TOKEN = "1519565919:AAE_oE1OmI7yiQ6hwfGJEtTFhTKP_doGdtM";
 
 const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
 
 bot.on('text', (msg) => {
-    msg.reply.text("lô cc!");
+    switch (msg.text) {
+        case "lô":
+            msg.reply.text("lô cc!");
+            break;
+        case "sủa":
+        case "bark":
+            msg.reply.text("gâu gâu!");
+            break;
+        default:
+            break;
+    }
 });
 
-bot.on(/(show\s)?kitty*/, (msg) => {
-    return msg.reply.photo('http://thecatapi.com/api/images/get');
+function getImage(url, callback) {
+    return https.get(url, (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            let dataObj = JSON.parse(data);
+            if (dataObj && callback) callback(dataObj.link);
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+}
+
+bot.on(/(kitty|cat|pussy)/, (msg) => {
+    return getImage('https://some-random-api.ml/img/cat', (url) => {
+        msg.reply.photo(url);
+    });
 });
+
+bot.on(/(dog|vang|cho|meme)/, (msg) => {
+    return getImage('https://some-random-api.ml/img/dog', (url) => {
+        msg.reply.photo(url);
+    });
+});
+
 
 bot.start();
